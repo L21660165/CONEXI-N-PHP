f284d1919276275b127a33fdb65ff2b3d36ecb8d
L21660165 <L21660165@matehuala.tecnm.mx>
2024-10-16 08:01:47 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi Página PHP</title>
+</head>
+<body>
+    <h1>Bienvenido a mi página PHP</h1>
+    <?php
+        echo "<p>Hola, mundo desde PHP!</p>";
+    ?>
+</body>
+</html>
